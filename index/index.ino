34312055ab99e6119d3222fc7d97f93cd59a4d44
15g16483 <stroudalex13@gmail.com>
2021-04-25 14:05:53 +0100

    Cycle 1.1 pass

--- index/index.ino
+++ index/index.ino
@@ -1,11 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
-  //THIS IS A GIT TEST!!!!
-  //THIS IS CODE
+//set up the pins:
+int echoPin = 9;
+int trigPin = 8;
 
+//declare other necessary variables
+float duration;
+
+
+void setup() {
+  //begin serial communication
+  Serial.begin(9600);
+
+  //configure the pins to input and output
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //reset the trig pin
+  digitalWrite(trigPin, LOW);
+
+  //activate the sensor
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  //read the pulse duration on echo pin
+  duration = pulseIn(echoPin, HIGH);
+
+  //display result to serial monitor
+  Serial.print("Echo Pin Input: ");
+  Serial.println(duration);
+
+  //delay for a second until the next reading
+  delay(1000);
 
 }
