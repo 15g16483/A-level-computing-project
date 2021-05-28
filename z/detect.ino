84e490392b952e3211e2cf65dc81ed4fb373ac4c
15g16483 <stroudalex13@gmail.com>
2021-05-28 12:04:09 +0100

    detect compile pass

--- z/detect.ino
+++ z/detect.ino
@@ -1,63 +1,31 @@
-//THIS IS TEMPORARY STORAGE FOR CYCLE 1 SCRIPT
+//function to be called to "ping" the environment from one sensor
+float detect(int dir){
+  
+  //echopins are 3,5,11
+  //trig pins are 2,4,12
 
-//set up the pins:
-int echoPin1 = 11;
-int echoPin2 = 3;
-int echoPin3 = 10;
-int echoPin4 = 9;
-int trigPin = 8;
-
-//declare other necessary variables
-float duration1,duration2,duration3,duration4,distance1,distance2,distance3,distance4;
-
-//function to be called to "ping" the environment
-void detect(){
-
-  //set up the pins:
-  int echoPin1 = 11;
-  int echoPin2 = 3;
-  int echoPin3 = 10;
-  int echoPin4 = 9;
-  int trigPin = 8;
-
-  //declare other necessary variables
-  float duration1,duration2,duration3,duration4,distance1,distance2,distance3,distance4;
+  //store pins in arrays for future reference
+  int echoPin[3] = {3,5,11};
+  int trigPin[3] = {2,4,12};
   
   //configure the pins to input and output
-  pinMode(echoPin1, INPUT);
-  pinMode(echoPin2, INPUT);
-  pinMode(echoPin3, INPUT);
-  pinMode(trigPin, INPUT);
-  //reset the trig pins
-    digitalWrite(trigPin, LOW);
+  pinMode(echoPin[dir], INPUT);
+  pinMode(trigPin[dir], OUTPUT);
   
-    //define duration vairables before hand to attempt synchronous functionality
-    duration1 = pulseIn(echoPin1, HIGH);
-    duration2 = pulseIn(echoPin2, HIGH);
-    duration3 = pulseIn(echoPin3, HIGH);
-    duration4 = pulseIn(echoPin4, HIGH);
+  //reset the trig pin
+  digitalWrite(trigPin[dir], LOW);
+
+  //activate sensor
+  digitalWrite(trigPin[dir], HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin[dir], LOW);
   
-    //activate sensors
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    
-    //calculate the distance from the duration
-    distance1 = 0.00017 * duration1;
-    distance2 = 0.00017 * duration2;
-    distance3 = 0.00017 * duration3;
-    distance4 = 0.00017 * duration4;
+  //find the duration of the pulse
+  float duration = pulseIn(echoPin[dir], HIGH);
+
   
-    //display result to serial monitor
-    Serial.print("Distance 1: ");
-    Serial.println(distance1);
-    Serial.print("Distance 2: ");
-    Serial.println(distance2);
-    Serial.print("Distance 3: ");
-    Serial.println(distance3);
-    Serial.print("Distance 4: ");
-    Serial.println(distance4);
+  //calculate the distance from the duration
+  float distance = 0.00017 * duration;
   
-    //delay for a second until the next reading
-    delay(1000);
+  return distance;
 };
